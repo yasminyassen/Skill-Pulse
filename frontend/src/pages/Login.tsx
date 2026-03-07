0957da3e602c7dda5ca064651f50ef2db8f79ecf
esraamahmoudd <147391506+esraamahmoudd@users.noreply.github.com>
2026-03-07 19:22:05 +0200

    Delete frontend directory

--- frontend/src/pages/Login.tsx
+++
@@ -1,9 +0,0 @@
-const Login = () => {
-    return (
-      <div>
-        <h1>SkillPulse Login</h1>
-        <p>GitHub OAuth login will be implemented here.</p>
-      </div>
-    );
-  };
-  export default Login;
